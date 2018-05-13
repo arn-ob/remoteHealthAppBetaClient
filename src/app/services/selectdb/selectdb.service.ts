1174b09b7774c454ecefa6ba7f7c4f1261aef06d
stupidArnob <arnobxtreme@gmail.com>
2018-05-13 17:46:17 +0600

    Graph Working perfectly

--- src/app/services/selectdb/selectdb.service.ts
+++ src/app/services/selectdb/selectdb.service.ts
@@ -5,7 +5,7 @@ export class SelectdbService {
   url = 'http://localhost:5000/';     // Local Requset
   url_aws = 'http://healthappserver.us-east-2.elasticbeanstalk.com/';  // Cloud Requset
 
-  select_db = this.url;
+  select_db = this.url_aws;
 
 
   constructor() { }
